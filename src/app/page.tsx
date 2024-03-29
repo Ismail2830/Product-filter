33e7ab42e8f348a755bf8c2b6091a562215209ea
Ismail2830 <ismailaitrehail2830@gmail.com>
2024-03-29 07:33:12 +0000

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,11 @@ import { useQuery } from "@tanstack/react-query";
 import { QueryResult } from "@upstash/vector";
 import axios from "axios";
 import { ChevronDown, Filter } from "lucide-react";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ProductState } from '@/lib/validators/product-validator'
+import { Slider } from "@/components/ui/slider";
+import debounce from 'lodash.debounce'
+import EmptyState from "@/components/Products/EmptyState";
 
 const SORT_OPTIONS = [
   { name: 'None', value: 'none' },
@@ -31,6 +34,33 @@ const COLOR_FILTERS = {
   ] as const,
 }
 
+const SIZE_FILTERS = {
+  id: 'size',
+  name: 'Size',
+  options: [
+    { value: 'S', label: 'S' },
+    { value: 'M', label: 'M' },
+    { value: 'L', label: 'L' },
+  ],
+} as const
+
+const PRICE_FILTERS = {
+  id: 'price',
+  name: 'Price',
+  options: [
+    { value: [0, 100], label: 'Any price' },
+    {
+      value: [0, 20],
+      label: 'Under 20€',
+    },
+    {
+      value: [0, 40],
+      label: 'Under 40€',
+    },
+    // custom option defined in JSX
+  ],
+} as const
+
 const SUBCATEGORIES = [
   { name: 'T-Shirts', selected: true, href: '#' },
   { name: 'Hoodies', selected: false, href: '#' },
@@ -49,13 +79,16 @@ export default function Home() {
   })
   console.log(filter);
 
-  const {data: products} = useQuery({
+  const {data: products, refetch } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await axios.post<QueryResult<TProduct>[]>(
         'http://localhost:3000/api/products', {
           filter: {
             sort: filter.sort,
+            color: filter.color,
+            price: filter.price.range,
+            size: filter.size,
             
           }
         }
@@ -63,6 +96,11 @@ export default function Home() {
       return data 
     },
   })
+
+  const onSubmit = () => refetch()
+
+  const debouncedSubmit = debounce(onSubmit, 400)
+  const _debouncedSubmit = useCallback(debouncedSubmit, [])
 
   const applyArrayFilter = ({
     category,
@@ -88,6 +126,8 @@ export default function Home() {
     _debouncedSubmit()
   }  
   
+  const minPrice = Math.min(filter.price.range[0], filter.price.range[1])
+  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1])
   
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -113,6 +153,7 @@ export default function Home() {
                     ...prev,
                     sort: option.value
                   }))
+                  _debouncedSubmit()
                 }}>{option.name}</button>
               ))}
             </DropdownMenuContent>
@@ -155,6 +196,7 @@ export default function Home() {
                                 value: option.value,
                               })
                             }}
+                            checked={filter.color.includes(option.value)}
                             className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
                           />
                           <label
@@ -167,12 +209,153 @@ export default function Home() {
                     </ul>
                   </AccordionContent>
                 </AccordionItem>
+                {/* size Filter */}
+                <AccordionItem value='size'>
+                  <AccordionTrigger className='py-3 text-sm text-gray-400 hover:text-gray-500'>
+                    <span className='font-medium text-gray-900'>Size</span>
+                  </AccordionTrigger>
+                  <AccordionContent className='pt-6 animate-none'>
+                    <ul className='space-y-4'>
+                      {SIZE_FILTERS.options.map((option, optionIdx) => (
+                        <li key={option.value} className="flex items-center">
+                          <input 
+                            type="checkbox"
+                            id={`size-${optionIdx}`}
+                            onChange={() => {
+                              applyArrayFilter({
+                                category: 'size',
+                                value: option.value,
+                              })
+                            }}
+                            checked={filter.size.includes(option.value)}
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                          htmlFor={`size-${optionIdx}`}
+                          className='ml-3 text-sm text-gray-600'>
+                          {option.label}
+                        </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+                {/* Price filter */}
+              <AccordionItem value='price'>
+                <AccordionTrigger className='py-3 text-sm text-gray-400 hover:text-gray-500'>
+                  <span className='font-medium text-gray-900'>Price</span>
+                </AccordionTrigger>
+
+                <AccordionContent className='pt-6 animate-none'>
+                  <ul className='space-y-4'>
+                    {PRICE_FILTERS.options.map((option, optionIdx) => (
+                      <li key={option.label} className='flex items-center'>
+                        <input
+                          type='radio'
+                          id={`price-${optionIdx}`}
+                          onChange={() => {
+                            setFilter((prev) => ({
+                              ...prev,
+                              price: {
+                                isCustom: false,
+                                range: [...option.value],
+                              },
+                            }))
+
+                            _debouncedSubmit()
+                          }}
+                          checked={
+                            !filter.price.isCustom &&
+                            filter.price.range[0] === option.value[0] &&
+                            filter.price.range[1] === option.value[1]
+                          }
+                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                        />
+                        <label
+                          htmlFor={`price-${optionIdx}`}
+                          className='ml-3 text-sm text-gray-600'>
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                    <li className='flex justify-center flex-col gap-2'>
+                      <div>
+                        <input
+                          type='radio'
+                          id={`price-${PRICE_FILTERS.options.length}`}
+                          onChange={() => {
+                            setFilter((prev) => ({
+                              ...prev,
+                              price: {
+                                isCustom: true,
+                                range: [0, 100],
+                              },
+                            }))
+
+                            _debouncedSubmit()
+                          }}
+                          checked={filter.price.isCustom}
+                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                        />
+                        <label
+                          htmlFor={`price-${PRICE_FILTERS.options.length}`}
+                          className='ml-3 text-sm text-gray-600'>
+                          Custom
+                        </label>
+                      </div>
+
+                      <div className='flex justify-between'>
+                        <p className='font-medium'>Price</p>
+                        <div>
+                          {filter.price.isCustom
+                            ? minPrice.toFixed(0)
+                            : filter.price.range[0].toFixed(0)}{' '}
+                          € -{' '}
+                          {filter.price.isCustom
+                            ? maxPrice.toFixed(0)
+                            : filter.price.range[1].toFixed(0)}{' '}
+                          €
+                        </div>
+                      </div>
+
+                      <Slider
+                        className={cn({
+                          'opacity-50': !filter.price.isCustom,
+                        })}
+                        disabled={!filter.price.isCustom}
+                        onValueChange={(range) => {
+                          const [newMin, newMax] = range
+
+                          setFilter((prev) => ({
+                            ...prev,
+                            price: {
+                              isCustom: true,
+                              range: [newMin, newMax],
+                            },
+                          }))
+
+                          _debouncedSubmit()
+                        }}
+                        value={
+                          filter.price.isCustom
+                            ? filter.price.range
+                            : DEFAULT_CUSTOM_PRICE
+                        }
+                        min={DEFAULT_CUSTOM_PRICE[0]}
+                        defaultValue={DEFAULT_CUSTOM_PRICE}
+                        max={DEFAULT_CUSTOM_PRICE[1]}
+                        step={5}
+                      />
+                    </li>
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
               </Accordion>
 
             </div>
             {/* Product grid */}
             <ul className='lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
-                {products ? products.map((product) => (
+                {products && products.length === 0 ? <EmptyState /> : products ? products.map((product) => (
                   <Product product={product.metadata!} /> )) : new Array(12)
                             .fill(null)
                             .map((_,i) => <ProductSkeleton key={i} />)  
